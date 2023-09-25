cca380f563f23a94f61bca6eb42e0dd2647f99c4
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-09-24 21:57:22 -0400

    iam testing

--- main.tf
+++ main.tf
@@ -29,8 +29,8 @@ provider "aws" {
   region = "us-west-2" #CHANGEME
 }
 
-resource "abbey_grant_kit" "IAM_membership1" {
-  name = "IAM_membership"
+resource "abbey_grant_kit" "IAM_membership2" {
+  name = "IAM_membership2"
   description = <<-EOT
     Grants membership to an IAM Group.
     This Grant Kit uses a single-step Grant Workflow that requires only a single reviewer
@@ -52,15 +52,15 @@ resource "abbey_grant_kit" "IAM_membership1" {
     # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
     location = "github://dougsillars/abbey-aws-iam/access.tf" #CHANGEME
     append = <<-EOT
-      resource "aws_iam_user_group_membership" "user_{{ .data.system.abbey.identities.aws_iam.name }}_group_${data.aws_iam_group.abbey-test.group_name}" {
+      resource "aws_iam_user_group_membership" "user_{{ .data.system.abbey.identities.aws_iam.name }}_group_${data.aws_iam_group.group1.group_name}" {
         user = "{{ .data.system.abbey.identities.aws_iam.name }}"
-        groups = ["${data.aws_iam_group.abbey-test.group_name}"]
+        groups = ["${data.aws_iam_group.group1.group_name}"]
       }
     EOT
   }
 }
 
-resource "abbey_identity" "abbey_test_user" {
+resource "abbey_identity" "user_1" {
   abbey_account = "doug.sillars@gmail.com"
   source = "aws_iam"
   metadata = jsonencode(
@@ -70,7 +70,7 @@ resource "abbey_identity" "abbey_test_user" {
   )
 }
 
-data "aws_iam_group" "abbey-test" {
+data "aws_iam_group" "group1" {
   group_name = "abbey-test"
 }
 
