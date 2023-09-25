277ab829bb867f991e4e6b1d585b54d2c64dc2fa
Abbey <no-reply@abbey.io>
2023-09-25 02:18:33 +0000

    doug.sillars@gmail.com requests access to: IAM_membership5

--- access.tf
+++ access.tf
@@ -2,3 +2,7 @@ resource "aws_iam_user_group_membership" "user_abbey_test_user_group_abbey-test"
   user = "abbey_test_user"
   groups = ["abbey-test"]
 }
+resource "aws_iam_user_group_membership" "user_abbey_test_user_group_abbey-test" {
+  user = "abbey_test_user"
+  groups = ["abbey-test"]
+}
